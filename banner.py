3d5f31f861a98f95e4b09a2a17914e9fb62988c5
eetnumakkelijk-mb <eetnumakkelijk@gmail.com>
2026-06-05 12:41:43 +0200

    Delete banner.py

--- banner.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def toon_banner():
-    st.markdown("🎨 *Jouw persoonlijke Gezonde Restjes Chef kookboek*")
-    st.image("https://openclipart.org", use_container_width=True)
